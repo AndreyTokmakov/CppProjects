003036b5d5a7bfdf48398682c96225d45d4cf20d
AndreyTokmakov <andtokm@yandex.ru>
2023-07-11 20:51:13 +0400

    11_07_2023

---
+++ LinuxProcessManager/ProcessManager.cpp
@@ -0,0 +1,236 @@
+/**============================================================================
+Name        : ProcessManager.cpp
+Created on  : 11.09.2023
+Author      : Tokmakov Andrei
+Version     : 1.0
+Copyright   : Your copyright notice
+Description : v
+============================================================================**/
+
+#include "ProcessManager.h"
+
+
+#include <optional>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <filesystem>
+#include <tuple>
+#include <ranges>
+
+#include <exception>
+#include <thread>
+#include <future>
+#include <mutex>
+#include <syncstream>
+#include <utility>
+
+#include <numeric>
+
+#include <utility>
+#include <vector>
+#include <any>
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <map>
+#include <algorithm>
+#include <array>
+#include <version>
+#include <concepts>
+#include <span>
+#include <cmath>
+#include <stack>
+#include <variant>
+#include <chrono>
+
+#include <QtCore>
+#include <QWidget>
+#include <QApplication>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QStatusBar>
+// #include <QDebug>
+#include <QFrame>
+#include <QIcon>
+#include <QMenu>
+#include <QMenuBar>
+#include <QStyle>
+#include <QStandardItem>
+#include <QHeaderView>
+#include <QTreeView>
+
+
+class MyThread : public QThread
+{
+public:
+    explicit MyThread(QString s) : name(std::move(s)) {
+    }
+
+    void run() override
+    {
+        for(int i = 1; i <= 5; ++i) {
+            qDebug() << this->name << " " << i;
+            //std::cout << this->name.toStdString() << " " << i << std::endl;
+        }
+    }
+
+private:
+    QString name;
+};
+
+
+class Application : public QApplication
+{
+public:
+    Application(int &argc, char **argv) : QApplication(argc, argv) {
+        enableDarkMode();
+    }
+
+private:
+    void enableDarkMode()
+    {
+#ifndef Q_OS_MACOS
+        qApp->setStyle("Fusion");
+        QPalette darkPalette = QPalette();
+        const QColor darkColor = QColor(65, 65, 65);
+        const QColor baseColor = QColor(48,48,48);
+        const QColor disabledColor = QColor(127,127,127);
+
+        darkPalette.setColor(QPalette::Window, darkColor);
+        darkPalette.setColor(QPalette::WindowText, Qt::white);
+        darkPalette.setColor(QPalette::Base, baseColor);
+        darkPalette.setColor(QPalette::AlternateBase, darkColor);
+        darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+        darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+        darkPalette.setColor(QPalette::Text, Qt::white);
+        darkPalette.setColor(QPalette::Disabled, QPalette::Text, disabledColor);
+        darkPalette.setColor(QPalette::Button, darkColor);
+        darkPalette.setColor(QPalette::ButtonText, Qt::white);
+        darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, disabledColor);
+        darkPalette.setColor(QPalette::BrightText, Qt::red);
+        darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+        darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, disabledColor);
+
+        qApp->setPalette(darkPalette);
+        qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+#endif
+    }
+};
+
+
+
+class Window : public QMainWindow
+{
+public:
+
+    Window()
+    {
+        /** Init TreeView:: **/
+        layout.addWidget(&treeView1);
+
+        auto rootNode = new QStandardItem("Root");
+        model.appendRow(rootNode);
+        auto firstNode = new QStandardItem("First");
+        rootNode->appendRow(firstNode);
+        firstNode->appendRow(new QStandardItem("Second"));
+        firstNode->appendRow(new QStandardItem("Third"));
+        auto fourthNode = new QStandardItem("Fourth");
+        rootNode->appendRow(fourthNode);
+        fourthNode->appendRow(new QStandardItem("Fifth"));
+        fourthNode->appendRow(new QStandardItem("Sixth"));
+        fourthNode->appendRow(new QStandardItem("Seventh"));
+
+        treeView1.setHeaderHidden(true);
+        treeView1.setModel(&model);
+        treeView1.expandAll();
+
+        /** Add status bar: **/
+        status->showMessage("Status bar...");
+
+        QMenu* menuFile = menu->addMenu("&File");
+        menuFile->addAction(style()->standardIcon(QStyle::StandardPixmap::SP_FileIcon),"&New",this, &Window::OnMenuItemClick);
+        menuFile->addAction(style()->standardIcon(QStyle::StandardPixmap::SP_DirOpenIcon), "&Open",this, &Window::OnMenuItemClick);
+
+        menuFile->addSeparator();
+
+        menuFile->addAction(style()->standardIcon(QStyle::StandardPixmap::SP_DialogSaveButton),"&Save", this, &Window::OnMenuItemClick);
+        menuFile->addAction("Save &As...", this,&Window::OnMenuItemClick);
+
+        menuFile->addSeparator();
+
+        menuFile->addAction("&Exit", this,&Window::OnMenuItemClick);
+
+        QMenu* menuEdit = menu->addMenu("&Edit");
+        menuEdit->addAction("&Undo", this,&Window::OnMenuItemClick);
+        menuEdit->addAction("&Redo", this,&Window::OnMenuItemClick);
+
+        menuEdit->addSeparator();
+
+        menuEdit->addAction(QIcon::fromTheme("edit-cut"), "&Cut",this, &Window::OnMenuItemClick);
+        menuEdit->addAction(QIcon::fromTheme("edit-copy"), "&Copy",this, &Window::OnMenuItemClick);
+        menuEdit->addAction(QIcon::fromTheme("edit-paste"), "&Paste",this, &Window::OnMenuItemClick);
+
+        menuEdit->addSeparator();
+
+        menuEdit->addAction("Select &All", this,&Window::OnMenuItemClick);
+
+        QMenu* menuHelp = menu->addMenu("&Help");
+        menuHelp->addAction("&About", this, &Window::OnMenuHelpAboutClick);
+
+
+        setCentralWidget(&frame);
+        setWindowTitle("Tree view example");
+        //resize(300, 300);
+
+        thread.start();
+        // thread.wait();
+    }
+
+private:
+    void OnMenuItemClick() {
+        status->showMessage("Status bar...wwwww");
+    }
+
+    void OnMenuHelpAboutClick() {
+        QMessageBox::about(this, "About", "MainMenu example.\nVersion 1.0.0\n\n@ 2020 by Gammasoft.");
+    }
+
+private:
+    QFrame frame;
+    QVBoxLayout layout{&frame};
+    QTreeView treeView1;
+    QStandardItemModel model;
+
+    MyThread thread {" A"};
+
+    const std::unique_ptr<QStatusBar> status { statusBar() };
+    const std::unique_ptr<QMenuBar> menu { menuBar() };
+};
+
+void runApp(int argc, char **argv)
+{
+    Application application(argc, argv);
+    Window window;
+    window.resize(1200, 800);
+    window.show();
+    QApplication::exec();
+}
+
+
+
+void ProcessManager::run(int argc, char **argv)
+{
+    runApp(argc, argv);
+
+}
+
+
+#include "ProcessManager.moc"
---
+++ LinuxProcessManager/ProcessManager.h
@@ -0,0 +1,19 @@
+/**============================================================================
+Name        : ProcessManager.h
+Created on  : 11.09.2023
+Author      : Tokmakov Andrei
+Version     : 1.0
+Copyright   : Your copyright notice
+Description : v
+============================================================================**/
+
+#ifndef CPPPROJECTS_PROCESSMANAGER_H
+#define CPPPROJECTS_PROCESSMANAGER_H
+
+class ProcessManager
+{
+public:
+    void run(int argc, char **argv);
+};
+
+#endif //CPPPROJECTS_PROCESSMANAGER_H
