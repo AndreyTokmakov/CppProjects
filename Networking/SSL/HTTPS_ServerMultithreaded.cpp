30d9d899d236b33c6a94bc27627a9f286de83ae3
AndreyTokmakov <andtokm@yandex.ru>
2023-08-31 20:40:15 +0400

    31_08_2023_01

--- Networking/SSL/HTTPS_ServerMultithreaded.cpp
+++ Networking/SSL/HTTPS_ServerMultithreaded.cpp
@@ -9,28 +9,15 @@ Description : HTTPS_ServerMultithreaded
 
 #include "HTTPS_ServerMultithreaded.h"
 
-
-
-#include <openssl/sha.h>
-#include <openssl/rsa.h>
-#include <openssl/rand.h>
-#include <openssl/engine.h>
-#include <openssl/hmac.h>
-#include <openssl/evp.h>
 #include <openssl/bio.h>
-#include <openssl/pem.h>
-#include <openssl/buffer.h>
 #include <openssl/err.h>
 #include <openssl/x509.h>
-#include <openssl/x509v3.h>
 #include <openssl/crypto.h>
-#include <openssl/err.h>
 #include <openssl/ssl.h>
 
 #include <iostream>
 #include <string_view>
 #include <memory>
-#include <vector>
 #include <array>
 #include <format>
 #include <thread>
@@ -104,6 +91,7 @@ namespace
         return errno;
     }
 
+    [[maybe_unused]]
     void printPeerCertificateInfo(SSL* ssl)
     {
         const std::unique_ptr<X509, CertificateDeleter> certX509 {
@@ -136,171 +124,153 @@ namespace
 
 namespace HTTPS_ServerMultithreaded
 {
-
-    constexpr std::string_view pageHTML {
-            "<html><head><title>AndTokmServer</title></head><body BGCOLOR='grey'>Welcome</body></html>"
-    };
-
-    const std::string response = std::string { "HTTP/1.1 200 OK\r\n"}
-            .append("Date: Wed, 11 Feb 2009 11:20:59 GMT\r\n")
-            .append("Server: AndTokmServer\r\n")
-            .append("X-Powered-By: PHP/5.2.4-2ubuntu5wm1\r\n")
-            .append("Last-Modified: Wed, 11 Feb 2009 11:20:59 GMT\r\n")
-            .append("Content-Language: ru\r\n")
-            .append("Content-Type: text/html; charset=utf-8\r\n")
-            .append("Content-Length: " + std::to_string(pageHTML.length()) + "\r\n")
-            .append("Connection: close\r\n\r\n")
-            .append(pageHTML)
-            .append("\r\n");
-
-    void processClientConnection(int clientSocket,
-                                 SSL_CTX* sslContext)
+    struct HTTPS_Server
     {
-        const SocketGuard clientSockGuard {clientSocket };
-        std::unique_ptr<SSL, decltype(&::SSL_free)> ssl {SSL_new(sslContext), SSL_free };
-        if (!ssl) {
-            PutSSLError("SSL_new");
-            return;
-        }
+        static inline constexpr std::string_view pageHTML {
+                "<html><head><title>AndTokmServer</title></head><body BGCOLOR='grey'>Welcome</body></html>"
+        };
 
-        // const X509* cert = SSL_get_peer_certificate(ssl.get());
-        // printPeerCertificateInfo(ssl.get());
+        const std::string response = std::string { "HTTP/1.1 200 OK\r\n"}
+                .append("Date: Wed, 11 Feb 2009 11:20:59 GMT\r\n")
+                .append("Server: AndTokmServer\r\n")
+                .append("X-Powered-By: PHP/5.2.4-2ubuntu5wm1\r\n")
+                .append("Last-Modified: Wed, 11 Feb 2009 11:20:59 GMT\r\n")
+                .append("Content-Language: ru\r\n")
+                .append("Content-Type: text/html; charset=utf-8\r\n")
+                .append("Content-Length: " + std::to_string(pageHTML.length()) + "\r\n")
+                .append("Connection: close\r\n\r\n")
+                .append(pageHTML)
+                .append("\r\n");
 
+        /** SSL Context **/
+        std::unique_ptr<SSL_CTX, decltype(&::SSL_CTX_free)> sslContext {
+            std::unique_ptr<SSL_CTX, decltype(&::SSL_CTX_free)> {nullptr, SSL_CTX_free }
+        };
 
-        if (const int result = SSL_set_fd(ssl.get(), clientSocket); 1 != result)
+        int processClientConnection(int clientSocket)
         {
-            PutSSLError("SSL_set_fd", result);
-            return;
-        }
-        else {
-            std::cerr << "OK. SSL_set_fd() = " << result << std::endl;
+            const SocketGuard clientSockGuard {clientSocket };
+            std::unique_ptr<SSL, decltype(&::SSL_free)> ssl {SSL_new(sslContext.get()), SSL_free };
+            if (!ssl) {
+                return PutSSLError("SSL_new");
+            }
+
+            // const X509* cert = SSL_get_peer_certificate(ssl.get());
+            // printPeerCertificateInfo(ssl.get());
+
+            if (const int result = SSL_set_fd(ssl.get(), clientSocket); 1 != result) {
+                return PutSSLError("SSL_set_fd", result);
+            }
+            if (const int result = SSL_accept(ssl.get()); 1 != result) {
+                return PutSSLError("SSL_accept", result);
+            }
+
+            constexpr size_t bytesToRead {44}; // FIXME
+            std::array<char, 1024> buffer {};
+            if (const int bytesRead = SSL_read(ssl.get(), buffer.data(), bytesToRead); bytesToRead != bytesRead)
+            {
+                std::cout << std::string_view {buffer.data(), bytesToRead} << std::endl;
+            }
+            else {
+                PutSSLError("SSL_read", bytesRead);
+            }
+
+
+            if (const int bytesWritten = SSL_write(ssl.get(), response.data(), response.length()); bytesWritten) {
+                std::cout << bytesWritten << " bytes send\n";
+            }
+            else {
+                PutSSLError("SSL_read", bytesWritten);
+            }
+
+            return SSL_shutdown(ssl.get());
         }
 
-        if (const int result = SSL_accept(ssl.get()); 1 != result)
-        {
-            PutSSLError("SSL_accept", result);
-            return;
-        }
-        else {
-            std::cerr << "OK. SSL_accept() = " << result << std::endl;
-        }
-
-        constexpr size_t bytesToRead {44}; // FIXME
-        std::array<char, 1024> buffer {};
-        if (const int bytesRead = SSL_read(ssl.get(), buffer.data(), bytesToRead); bytesToRead != bytesRead)
-        {
-            std::cout << std::string_view {buffer.data(), bytesToRead} << std::endl;
-        }
-        else {
-            PutSSLError("SSL_read", bytesRead);
-        }
-
-
-        if (const int bytesWritten = SSL_write(ssl.get(), response.data(), response.length()); bytesWritten) {
-            std::cout << bytesWritten << " bytes send\n";
-        }
-        else {
-            PutSSLError("SSL_read", bytesWritten);
-        }
-
-        SSL_shutdown(ssl.get());
-    }
-
-    int runServer()
-    {
-        const int serverSocket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (INVALID_SOCKET == serverSocket) {
-            Error("Failed to create socket");
-        }
-
-        const SocketGuard guard { serverSocket };
-
+        bool initSSL()
         {
             SSL_library_init();
             SSL_load_error_strings();
             OpenSSL_add_ssl_algorithms();
-        }
 
-        const SSL_METHOD *method = SSLv23_server_method();
-        std::unique_ptr<SSL_CTX, decltype(&::SSL_CTX_free)> sslContext {SSL_CTX_new(method), SSL_CTX_free };
-        if (!sslContext) {
-            return PutSSLError("SSL_CTX_new");
-        }
+            const SSL_METHOD *method = SSLv23_server_method();
+            sslContext = std::unique_ptr<SSL_CTX, decltype(&::SSL_CTX_free)>{SSL_CTX_new(method), SSL_CTX_free };
+            if (!sslContext) {
+                PutSSLError("SSL_CTX_new");
+                return false;
+            }
 
-        {
             if (1 == SSL_CTX_use_certificate_file(sslContext.get(), certFile.data(), SSL_FILETYPE_PEM)) {
                 std::cout << "CERTIFICATE INITIALISED" << std::endl;
+            } else {
+                return false;
             }
+
             if (1 == SSL_CTX_use_PrivateKey_file(sslContext.get(), keyFile.data(), SSL_FILETYPE_PEM)) {
                 std::cout << "KEY INITIALISED" << std::endl;
+            } else {
+                return false;
             }
+
             if (1 == SSL_CTX_check_private_key(sslContext.get())) {
                 std::cout << "KEY VALIDATED" << std::endl;
-            }
-        }
-
-        constexpr uint16_t port { 52525 };
-        constexpr std::string_view host {"0.0.0.0"};
-        sockaddr_in server {PF_INET, htons(port), {.s_addr = inet_addr(host.data())}, {}};
-
-        if (SOCKET_ERROR == ::bind(serverSocket, reinterpret_cast<sockaddr*>(&server), sizeof(server))) {
-            return Error("Failed to bind socket");
-        }
-
-        constexpr uint16_t backlog { 10 };
-        if (SOCKET_ERROR == ::listen(serverSocket, backlog)) {
-            return Error("Failed to Listen the socket.");
-        }
-
-        std::cout << std::format("Running on https://{}:{}", host, port) << std::endl;
-
-        sockaddr_in clientAddr {};
-        socklen_t addLen { sizeof(clientAddr) };
-
-        while (true)
-        {
-            std::cout << "\nWaiting for next connection ....\n";
-            const int clientSocket = ::accept(serverSocket, reinterpret_cast<sockaddr*>(&clientAddr), &addLen);
-            if (INVALID_SOCKET == clientSocket) {
-                Error("Failed to create client socket");
-                continue;
+            } else {
+                return false;
             }
 
-            std::cout << "Client connected " << inet_ntoa(clientAddr.sin_addr) << ':' << htons(clientAddr.sin_port) << std::endl;
-
-            std::thread T {processClientConnection, clientSocket, sslContext.get()};
-            T.detach();
+            return true;
         }
-    }
-};
 
-struct Worker
-{
-    void task()
-    {
-        int i = 0;
-        while (true)
+        int start()
         {
-            std::cout << ++i << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-    }
+            if (!initSSL())
+                return -1;
 
-    void start()
-    {
-        for (int i = 0; i < 5; ++i)
-        {
-            std::thread T {&Worker::task, this};
-            // T.join();
-            T.detach();
+            const int serverSocket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if (INVALID_SOCKET == serverSocket) {
+                Error("Failed to create socket");
+            }
+
+            const SocketGuard guard { serverSocket };
+
+            constexpr uint16_t port { 52525 };
+            constexpr std::string_view host {"0.0.0.0"};
+            sockaddr_in server {PF_INET, htons(port), {.s_addr = inet_addr(host.data())}, {}};
+
+            if (SOCKET_ERROR == ::bind(serverSocket, reinterpret_cast<sockaddr*>(&server), sizeof(server))) {
+                return Error("Failed to bind socket");
+            }
+
+            constexpr uint16_t backlog { 10 };
+            if (SOCKET_ERROR == ::listen(serverSocket, backlog)) {
+                return Error("Failed to Listen the socket.");
+            }
+
+            std::cout << std::format("Running on https://{}:{}", host, port) << std::endl;
+
+            sockaddr_in clientAddr {};
+            socklen_t addLen { sizeof(clientAddr) };
+
+            while (true)
+            {
+                std::cout << "\nWaiting for next connection ....\n";
+                const int clientSocket = ::accept(serverSocket, reinterpret_cast<sockaddr*>(&clientAddr), &addLen);
+                if (INVALID_SOCKET == clientSocket) {
+                    Error("Failed to create client socket");
+                    continue;
+                }
+
+                std::cout << "Client connected " << inet_ntoa(clientAddr.sin_addr)
+                          << ':' << htons(clientAddr.sin_port) << std::endl;
+
+                std::thread T { &HTTPS_Server::processClientConnection, this, clientSocket};
+                T.detach();
+            }
         }
-    }
-};
+    };
+}
 
 void HTTPS_ServerMultithreaded::TestAll()
 {
-    // Worker{}.start();
-    // std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    runServer();
+    HTTPS_Server server;
+    server.start();
 };
