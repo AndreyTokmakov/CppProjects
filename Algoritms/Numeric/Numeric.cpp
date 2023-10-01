edf5a4a1e93e75a5e25293e7280785c2823d1f5d
AndreyTokmakov <andtokm@yandex.ru>
2023-10-01 21:16:11 +0400

    01_10_2023__003

--- Algoritms/Numeric/Numeric.cpp
+++ Algoritms/Numeric/Numeric.cpp
@@ -1285,7 +1285,7 @@ namespace Numeric {
 
     //--------------------------------------------------------------------------------------//
 
-    void find_longest_increasing_subsequence(const std::vector<int>& Numeric) {
+    size_t _longest_increasing_subsequence_1(const std::vector<int>& Numeric) {
         // create an empty ordered set S. ith element in S is defined as the
         // smallest integer that ends an increasing sequence of length i
         std::set<int> tmp;
@@ -1296,19 +1296,61 @@ namespace Numeric {
             // 2. Ignore the current element if already present in the set
             if (ok && tmp.end() != std::next(iter))
                 tmp.erase(std::next(iter));
-
-            std::cout << "[" << tmp << "]\n";;
         }
-
-        std::cout << "size = " << tmp.size() << ": " << tmp << std::endl;
+        return tmp.size();
     }
 
-    // TODO: FIX ME. ERROR with  {  2, 3, 1  }
-    void Find_Longest_Increasing_Subsequence() {
-        const std::vector<int> Numeric = {4, 5, 6, 1, 2 };
-        // const std::vector<int> Numeric = { 1, 22, 44, 5, 10 , 234, 13, 22};
+    /** Solution:
+    The O(nlogn) solution is less obvious.
 
-        find_longest_increasing_subsequence(Numeric);
+    If we consider building a subsequence iteratively, we have two situations.
+    Either the next element is strictly higher, in which case we can simply append it.
+
+    If it is not, we still want to remember it and we can do that by replacing the next higher value in the subsequence.
+
+    This works because we only ever decrease values this way:
+    - we won't break previous subsequences this way
+    - we won't accidentally skip over potential subsequences
+    **/
+
+    size_t _longest_increasing_subsequence_2(const std::vector<int>& numbers)
+    {
+        std::vector<int> path;
+        for (int64_t i = 0; i < std::ssize(numbers); ++i) {
+            // Current element is higher than the tail of the path.
+            if (path.empty() || path.back() < numbers[i]) {
+                path.push_back(numbers[i]);
+                continue;
+            }
+
+            // Find the element to overwrite
+            auto it = std::upper_bound(path.begin(), path.end(),numbers[i],[](int l, int r) {
+                return l <= r;
+            });
+
+            // it != path.end() because path.back() >= nums[i];
+            *it = numbers[i];
+        }
+        return path.size();
+    }
+
+    void Find_Longest_Increasing_Subsequence()
+    {
+        std::vector<std::pair<std::vector<int>, size_t>> testData {
+                {{ 1, 2, 3 }, 3},
+                {{ 3, 2, 1 }, 1},
+                {{ 2, 8, 4, 1, 9, 3, 5}, 3},
+                {{2, 1, 4, 3, 6, 5, 8, 7, 9}, 5},
+                {{1, 1, 1, 1}, 1},
+                {{10, 22, 9, 33, 21, 50, 41, 60, 80}, 6},
+                {{1, 2, 2, 3, 4, 4}, 4}
+        };
+        for (const auto& [data, expectedResult]: testData)
+        {
+            std::cout << _longest_increasing_subsequence_1(data) << " | "
+                      << _longest_increasing_subsequence_2(data) << " | "
+                      << expectedResult << std::endl;
+        }
     }
 
     //--------------------------------------------------------------------------------------//
