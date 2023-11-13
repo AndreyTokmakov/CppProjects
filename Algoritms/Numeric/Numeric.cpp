16c9aa4461e5f3e56f9d3b9d392d524579223c94
AndreyTokmakov <andtokm@yandex.ru>
2023-11-13 06:41:15 +0400

    13_11_2023__02

--- Algoritms/Numeric/Numeric.cpp
+++ Algoritms/Numeric/Numeric.cpp
@@ -3208,6 +3208,65 @@ namespace Numeric
     }
 }
 
+namespace Numeric::Boundaries
+{
+    size_t max_area(const std::vector<long>& height)
+    {
+        size_t max = 0;
+        for (size_t left = 0, right = height.size() - 1; left != right;) {
+            max = std::max(max, std::min(height[left], height[right]) * (right - left));
+            if (height[left] <= height[right])
+                ++left;
+            else
+                --right;
+        }
+        return max;
+    }
+
+    long max_area_iter(const std::vector<long>& height)
+    {
+        std::vector<long>::const_iterator l = height.begin();
+        std::vector<long>::const_iterator r = std::prev(height.end());
+        long max = 0;
+        while (l != r) {
+            max = std::max(max, std::min(*l,*r)*(r-l));
+            if (*l <= *r)
+                ++l;
+            else
+                --r;
+        }
+        return max;
+    }
+
+    /**
+     * Problem
+     * Given information about the height of a boundary at each position as std::vector<int64_t>,
+     * determine the maximum area formed between any two boundaries.
+     * The area is the minimum height of the two boundaries multiplied by the distance.
+     *
+     * Solution:
+     * If we switch one of the boundaries, we are decreasing the distance, so the only way to improve
+     * the area is to switch the lower of the two boundaries.
+     * Switching the higher one decreases the distance without improving the height
+     * (because the height is dictated by the lower of the two boundaries).
+     * Therefore, to get the maximum, we can repeatedly apply this logic, keeping track of the maximum area as we go.
+    **/
+
+    void Maximum_Area_Between_Boundaries()
+    {
+        for (const auto& [heights, area_expected]: std::vector<std::pair<std::vector<long>, long>> {
+                {{1,1}, 1}, {{1,9,1}, 2}, {{1,3,3,1},3}, {{1,3,1,3,1},6},
+                {{1,3,8,10,3,1},9}, {{1,3,8,8,3,1},9},
+                {{1,3,10,10,3,1},10}, {{1,2,5,3,2,12,1,3,7,8,2}, 35}
+        })
+        {
+            std::cout << max_area(heights) << " " << max_area_iter(heights)
+                      << ", Expected: " << area_expected << std::endl;
+        }
+    }
+
+}
+
 void Numeric::TEST_ALL()
 {
     // Numeric::isPowerOf2();
@@ -3289,7 +3348,7 @@ void Numeric::TEST_ALL()
     // Numeric::Find_K_MissingNumber();
     // Numeric::Find_Smallest_Missing_Positive_Number();
     // Numeric::Find_Repeating_And_Missing();
-    Numeric::FindTheDuplicateValue();
+    // Numeric::FindTheDuplicateValue();
 
     // Numeric::printSortedSquaredNumber_InSortedArray();
 
@@ -3315,6 +3374,8 @@ void Numeric::TEST_ALL()
     // Numeric::IsReversedEquals();
     // Numeric::ReverseToMakeEqual();
     // Numeric::MaxSum_of_NonConsecutive_Elements_In_Array();
+
+    Boundaries::Maximum_Area_Between_Boundaries();
 
     // Random::BiasedCoin();
 
