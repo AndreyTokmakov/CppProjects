4fe170b21a9f020e3a5dd045c245a62122c2551f
AndreyTokmakov <andtokm@yandex.ru>
2024-02-03 21:51:26 +0400

    03_02_2024__002

--- Algoritms/Numeric/Numeric.cpp
+++ Algoritms/Numeric/Numeric.cpp
@@ -3474,7 +3474,43 @@ namespace Numeric
 
         std::cout << result << std::endl;
     }
+}
 
+namespace Numeric
+{
+    double median_of_two_sorted_arrays(const std::vector<int>& nums1,
+                                       const std::vector<int>& nums2)
+    {
+        const int size1 = std::ssize(nums1), size2 = std::ssize(nums2);
+        const int steps = (size1 + size2) / 2;
+        int m1 = 0, m2 = 0;
+        for (int n = 0, idx1 = 0, idx2 = 0; n <= steps; ++n)
+        {
+            m1 = m2;
+            if (size1 > idx1 && size2 > idx2)
+                m2 = nums1[idx1] > nums2[idx2] ? nums2[idx2++] : nums1[idx1++];
+            else
+                m2 = size1 > idx1 ? nums1[idx1++] : nums2[idx2++];
+        }
+
+        return (size1 + size2) % 2 == 1 ? static_cast<double>(m2) : static_cast<double>(m1 + m2) / 2;
+    }
+
+    /**
+    Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+    The overall run time complexity should be O(log (m+n)).
+
+    Example 1:    Input: nums1 = [1,3], nums2 = [2]  --> Result: 2.00000
+                  Explanation: merged array = [1,2,3] and median is 2.
+
+    Example 2:    Input: nums1 = [1,2], nums2 = [3,4] --> Result: 2.50000
+                  Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+    */
+    void Median_of_Two_Sorted_Arrays()
+    {
+        std::cout << median_of_two_sorted_arrays({1,3}, {2}) << std::endl;
+        std::cout << median_of_two_sorted_arrays({1,2}, {3,4}) << std::endl;
+    }
 }
 
 void Numeric::TestAll()
@@ -3563,7 +3599,7 @@ void Numeric::TestAll()
 
     // Numeric::Find_Smallest_Missing_Positive_Number();
     // Numeric::Find_Repeating_And_Missing();
-    Numeric::FindTheDuplicateValue();
+    // Numeric::FindTheDuplicateValue();
 
     // Numeric::printSortedSquaredNumber_InSortedArray();
 
@@ -3597,6 +3633,8 @@ void Numeric::TestAll()
     // Numeric::Rank();
 
     // Random::BiasedCoin();
+
+    Median_of_Two_Sorted_Arrays();
 
     // Numeric::Min_Length_SubArray_WithSameDegree();  // Degree_Of_Array
                                                     // degreeOfArray: With same occurrences of duplicated elements
--- Tests/Algorithms/Algorithms.cpp
+++ Tests/Algorithms/Algorithms.cpp
@@ -960,6 +960,43 @@ namespace Algorithms::Strings
     }
 }
 
+namespace Algorithms::Numbers
+{
+    double median_of_two_sorted_arrays(const std::vector<int>& nums1,
+                                       const std::vector<int>& nums2)
+    {
+        const int size1 = std::ssize(nums1), size2 = std::ssize(nums2);
+        const int steps = (size1 + size2) / 2;
+        int m1 = 0, m2 = 0;
+        for (int n = 0, idx1 = 0, idx2 = 0; n <= steps; ++n)
+        {
+            m1 = m2;
+            if (size1 > idx1 && size2 > idx2)
+                m2 = nums1[idx1] > nums2[idx2] ? nums2[idx2++] : nums1[idx1++];
+            else
+                m2 = size1 > idx1 ? nums1[idx1++] : nums2[idx2++];
+        }
+
+        return (size1 + size2) % 2 == 1 ? static_cast<double>(m2) : static_cast<double>(m1 + m2) / 2;
+    }
+
+    /**
+    Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+    The overall run time complexity should be O(log (m+n)).
+
+    Example 1:    Input: nums1 = [1,3], nums2 = [2]  --> Result: 2.00000
+                  Explanation: merged array = [1,2,3] and median is 2.
+
+    Example 2:    Input: nums1 = [1,2], nums2 = [3,4] --> Result: 2.50000
+                  Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+    */
+    void Median_of_Two_Sorted_Arrays()
+    {
+        std::cout << median_of_two_sorted_arrays({1,3}, {2}) << std::endl;
+        std::cout << median_of_two_sorted_arrays({1,2}, {3,4}) << std::endl;
+    }
+}
+
 namespace Algorithms::Bits
 {
     template<typename T>
@@ -1286,6 +1323,7 @@ void Algorithms::TestAll()
     // Numbers::Rank();
     // Numbers::Degree_Of_Array();
     // Numbers::topKFrequent();
+    Numbers::Median_of_Two_Sorted_Arrays();
 
     // Strings::FindCommon_PrefixAndPostfix();
     // Strings::FindLastNotOf__Benchmark();
@@ -1301,7 +1339,7 @@ void Algorithms::TestAll()
 
     // Algorithms::Sqrt();
 
-    Contains();
+    // Contains();
 
     // CalcParentheses_3_BRacket();
 
