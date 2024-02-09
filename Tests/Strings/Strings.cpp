875fed8cb77532c3a8fd3dd47a3e6183b9c6ab71
AndreyTokmakov <andtokm@yandex.ru>
2024-02-09 19:03:33 +0400

    09_01_2024__001

--- Tests/Strings/Strings.cpp
+++ Tests/Strings/Strings.cpp
@@ -11,24 +11,137 @@ Description : Strings texts and experiments
 
 #include <iostream>
 #include <string_view>
-#include <vector>
 #include <list>
+#include <vector>
+#include <stdint.h>
+#include <iomanip>
 #include <algorithm>
+#include <array>
+
 
 namespace
 {
     template<typename T>
-    std::ostream &operator<<(std::ostream &ostr, const std::vector<T> &list) {
+    std::ostream &operator<<(std::ostream &stream, const std::vector<T> &list)
+    {
         for (const auto &i: list)
-            ostr << i << ' ';
-        return ostr;
+            stream << i << ' ';
+        return stream;
     }
 
     template<typename T>
-    std::ostream &operator<<(std::ostream &ostr, const std::list<T> &list) {
+    std::ostream &operator<<(std::ostream &stream, const std::list<T> &list)
+    {
         for (const auto &i: list)
-            ostr << " " << i;
-        return ostr;
+            stream << " " << i;
+        return stream;
+    }
+}
+
+
+namespace Strings::Utilities
+{
+    std::vector<std::string> split(std::string_view input,
+                                   std::string_view delimiter = " ")
+    {
+        std::vector<std::string> output;
+        for (size_t first = 0; first < input.size(); ) {
+            const auto second = input.find_first_of(delimiter, first);
+            if (first != second)
+                output.emplace_back(input.substr(first, second - first));
+            if (second == std::string_view::npos)
+                break;
+            first = second + 1;
+        }
+        return output;
+    }
+
+    // TODO: Check for performance: delimiter : std::string ---> std::string_view
+    [[nodiscard]]
+    std::vector<std::string> split(const std::string &str,
+                                   const size_t partsExpected = 10,
+                                   const std::string& delimiter = std::string {";"})
+    {
+        std::vector<std::string> parts{};
+        parts.reserve(partsExpected);
+        size_t pos = 0, prev = 0;
+        while ((pos = str.find(delimiter, prev)) != std::string::npos) {
+            parts.emplace_back(str, prev, pos - prev);
+            prev = pos + delimiter.length();
+        }
+        parts.emplace_back(str, prev, str.length() - prev);
+        return parts;
+    }
+
+    // TODO: Check for performance: delimiter : std::string ---> std::string_view
+    void split_to(const std::string &str,
+                  std::vector<std::string_view>& parts,
+                  const std::string& delimiter = std::string {";"})
+    {
+        parts.clear();
+        size_t pos = 0, prev = 0;
+        while ((pos = str.find(delimiter, prev)) != std::string::npos) {
+            parts.emplace_back(str.data() + prev, pos - prev);
+            prev = pos + delimiter.length();
+        }
+        parts.emplace_back(str.data() + prev, str.length() - prev);
+    }
+
+    std::string_view strip(const std::string& str)
+    {
+        std::string::size_type start = 0, end = str.length() - 1;
+        while (str.length() > start && str[start] == ' ') { ++start; }
+        while (end && str[end] == ' ') { --end; }
+        return std::string_view {str.data() + start, end - start + 1};
+    }
+
+    void strip(std::string& str)
+    {
+        std::string::size_type start = 0, length  = str.length();
+        while (length > start && str[start] == ' ') { ++start; }
+        str.erase(0, start);;
+
+        std::string::size_type end = length - start - 1;
+        while (end && str[end] == ' ') { --end; }
+        str.erase(end + 1, length - start - end);
+
+        str.shrink_to_fit();
+    }
+
+    // Remove ' ', '\t', '\n', '\r', '\n' symbols from the END and BEGINING of the string
+    void strip_ex(std::string& str)
+    {
+        constexpr std::array<char, 5> symbols { ' ', '\t', '\n', '\r', '\n'};
+
+        std::string::size_type start = 0, length  = str.length();
+        while (length > start && std::any_of(symbols.cbegin(), symbols.cend(), [&](const char c) {
+            return c == str[start]; })) { ++start; }
+        str.erase(0, start);;
+
+        std::string::size_type end = length - start - 1;
+        while (end  && std::any_of(symbols.cbegin(), symbols.cend(), [&](const char c) {
+            return c == str[end]; })) { --end; }
+        str.erase(end + 1, length - start - end);
+
+        str.shrink_to_fit();
+    }
+
+    constexpr std::array<char, 256> toExclude = []() -> std::array<char, 256> {
+        std::array<char, 256> tmp {};
+        for (const char c: {'\t', '\n', '\r', '\n'})
+            tmp[c] = 1;
+        return tmp;
+    } ();
+
+    void remove_chars_from_string(std::string& str)
+    {
+        size_t index = 0;
+        for (char c: str) {
+            if (0 == toExclude[c])
+                str[index++] = c;
+        }
+        str.resize(index);
+        str.shrink_to_fit();
     }
 }
 
@@ -101,32 +214,64 @@ namespace Strings::Literals
     }
 }
 
-namespace Strings::SplitTests
+namespace Strings::UtilitiesTests
 {
-    std::vector<std::string> split(std::string_view input,
-                                   std::string_view delims = " ")
-    {
-        std::vector<std::string> output;
-        for (size_t first = 0; first < input.size(); ) {
-            const auto second = input.find_first_of(delims, first);
-            if (first != second)
-                output.emplace_back(input.substr(first, second - first));
-            if (second == std::string_view::npos)
-                break;
-            first = second + 1;
-        }
-        return output;
-    }
-
+    using namespace Strings::Utilities;
 
     void split_test_1()
     {
-        const std::string text { "11_22_33_44" };
-        const std::vector<std::string> parts = split(text, "_");
 
-        std::cout << parts << std::endl;
+        const std::string text { "11_22_33_44" };
+
+        {
+            const std::vector<std::string> parts = split(text, "_");
+            std::cout << parts << std::endl;
+        }
+
+        {
+            const std::vector<std::string> parts = split(text, 10,"_");
+            std::cout << parts << std::endl;
+        }
+
+        {
+            std::vector<std::string_view> parts;
+            split_to(text, parts,"_");
+            std::cout << parts << std::endl;
+        }
     }
 
+    void strip_string_test()
+    {
+        const std::string str1 { "  A good   examplE    " };
+        std::cout << strip(str1) << std::endl;
+    }
+
+    void trim_string_test()
+    {
+        std::string str1 { "  A good   examplE    " };
+
+        std::cout << std::quoted(str1) << std::endl;
+        strip(str1);
+        std::cout << std::quoted(str1) << std::endl;
+    }
+
+    void strip_ex_string_test()
+    {
+        std::string str1 { "\t\t  A good   examplE    \n\t\n" };
+
+        std::cout << std::quoted(str1) << std::endl;
+        strip_ex(str1);
+        std::cout << std::quoted(str1) << std::endl;
+    }
+
+    void remove_chars_from_string_test()
+    {
+        std::string str1 { "\t\t  A good   examplE    \n\t\n" };
+
+        std::cout << std::quoted(str1) << std::endl;
+        remove_chars_from_string(str1);
+        std::cout << std::quoted(str1) << std::endl;
+    }
 }
 
 void Strings::TestAll()
@@ -134,5 +279,16 @@ void Strings::TestAll()
     // GetStringLengthAsCompileTime();
     // std::cout << sizeof(std::string) << std::endl;
 
-    SplitTests::split_test_1();
+    // UtilitiesTests::split_test_1();
+    // UtilitiesTests::strip_string_test();
+    // UtilitiesTests::strip_ex_string_test();
+    UtilitiesTests::remove_chars_from_string_test();
+    // UtilitiesTests::trim_string_test();
+
+
+    /*
+    constexpr std::array<char, 5> symbols { ' ', '\t', '\n', '\r', '\n'};
+    const bool result = std::any_of(symbols.cbegin(), symbols.cend(), [](const char c) { return c == ' '; });
+    std::cout << std::boolalpha << result << std::endl;
+    */
 };
--- Tests/main.cpp
+++ Tests/main.cpp
@@ -842,7 +842,7 @@ int main([[maybe_unused]] int argc,
     // CallFunctionByName::Test();
     // ReturnClass_MemberRef_CopyCTor::tests();
 
-    Algorithms::TestAll();
+    // Algorithms::TestAll();
     // AutoTests::TestAll();
     // Cpp23_Features::TestAll();
     // Concepts::TestAll();
@@ -878,7 +878,7 @@ int main([[maybe_unused]] int argc,
     // Convertaion_UTF8_UTF32::TestAll();    // Encoding
     // Unicode::TestAll();                   // Encoding
     // StringUtils::TestAll();               // Encoding
-    // Strings::TestAll();
+    Strings::TestAll();
     // Performance::TestAll();
     // BinaryAnalyzer::TestAll();
     // ThinkCell::IntervalMapTest();
