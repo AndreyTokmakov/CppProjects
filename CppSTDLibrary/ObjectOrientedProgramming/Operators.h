d4e967f63218be96daad0ca47df95e09cc403e89
AndreyTokmakov <andtokm@yandex.ru>
2024-04-07 22:29:28 +0400

    07_04_2024__004

---
+++ CppSTDLibrary/ObjectOrientedProgramming/Operators.h
@@ -0,0 +1,17 @@
+/**============================================================================
+Name        : Operators.cpp
+Created on  : 07.05.2024
+Author      : Tokmakov Andrei
+Version     : 1.0
+Copyright   : Your copyright notice
+Description : C++ OPP Operators
+============================================================================**/
+
+#ifndef CPPPROJECTS_OPERATORS_H
+#define CPPPROJECTS_OPERATORS_H
+
+namespace Operators {
+    void TestAll();
+}
+
+#endif //CPPPROJECTS_OPERATORS_H
