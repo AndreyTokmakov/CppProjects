8631aa270ee209e42483b1fea1ab349cc59eaacd
AndreyTokmakov <andtokm@yandex.ru>
2024-07-20 08:24:53 +0400

    20_07_2024__001

--- Multithreding/Threads/StoppingThreads.cpp
+++ Multithreding/Threads/StoppingThreads.cpp
@@ -14,27 +14,27 @@ Description : StoppingThreads.cpp
 #include <syncstream>
 #include <future>
 #include <vector>
+#include <optional>
 
 
 using namespace std::literals; // for duration literals
 
 
-namespace StoppingThreads
+namespace StoppingThreads::BasicExamples
 {
-    void worker(std::stop_token stop_token)
+    void worker(const std::stop_token &token)
     {
-        int counter {0};
-        while (!stop_token.stop_requested()) {
+        int counter{0};
+        while (!token.stop_requested()) {
             std::osyncstream(std::cout) << ++counter << ' ' << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds (250));
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
         }
         std::osyncstream(std::cout) << "Done\n";
     }
 
-
     void SimpleExample()
     {
-        std::jthread job = std::jthread([](const std::stop_token& token){
+        std::jthread job = std::jthread([](const std::stop_token &token) {
             while (!token.stop_requested()) {
                 std::osyncstream(std::cout) << "Thread: Doing some work . . . . . \n";
                 std::this_thread::sleep_for(250ms);
@@ -42,7 +42,7 @@ namespace StoppingThreads
             std::osyncstream(std::cout) << "Thread: Stopped!!!\n";
         });
 
-        std::this_thread::sleep_for(std::chrono::seconds (1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         std::osyncstream(std::cout) << "Main  : Stopping thread.\n";
         job.request_stop(); // request stop
 
@@ -52,9 +52,9 @@ namespace StoppingThreads
 
     void SimpleExample_StopCallback()
     {
-        std::jthread job = std::jthread([](const std::stop_token& token){
-            std::atomic_flag isStopRequested {false};
-            std::stop_callback callback(token, [&isStopRequested]{
+        std::jthread job = std::jthread([](const std::stop_token &token) {
+            std::atomic_flag isStopRequested{false};
+            std::stop_callback callback(token, [&isStopRequested] {
                 std::osyncstream(std::cout) << "Thread: Observed stop request\n";
                 isStopRequested.test_and_set();
             });
@@ -66,7 +66,7 @@ namespace StoppingThreads
             std::osyncstream(std::cout) << "Thread: Stopped!!!\n";
         });
 
-        std::this_thread::sleep_for(std::chrono::seconds (1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         std::osyncstream(std::cout) << "Main  : Stopping thread.\n";
         job.request_stop(); // request stop
 
@@ -84,7 +84,7 @@ namespace StoppingThreads
 
         Resource resource;
 
-        std::jthread job = std::jthread([&resource](const std::stop_token& token){
+        std::jthread job = std::jthread([&resource](const std::stop_token &token) {
             std::osyncstream(std::cout) << "Thread: Wait until resource is ready, or stop was requested\n";
 
             std::unique_lock lock(resource.mux);
@@ -99,7 +99,7 @@ namespace StoppingThreads
             std::osyncstream(std::cout) << "Thread: Stopped!!!\n";
         });
 
-        std::this_thread::sleep_for(std::chrono::seconds (1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
 
         bool stopWithRequest = false;
@@ -117,9 +117,36 @@ namespace StoppingThreads
         std::osyncstream(std::cout) << "Main  : Done.\n";
     }
 
+    void StopMultipleThreads()
+    {
+        auto func = [](const std::stop_token &stop_token) {
+            int counter{0};
+            const auto thread_id = std::this_thread::get_id();
+
+            std::stop_callback callBack(stop_token, [&counter, thread_id] {
+                std::osyncstream(std::cout) << "(Stop callback) id: " << thread_id << "; counter: " << counter << '\n';
+            });
+
+            while (!stop_token.stop_requested()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(250));
+                ++counter;
+            }
+
+            std::osyncstream(std::cout) << "Thread id: " << thread_id << " done\n";
+        };
+
+        std::vector<std::jthread> tasks(10);
+        for (std::jthread &T: tasks)
+            T = std::jthread(func);
+
+        std::this_thread::sleep_for(1s);
+        for (std::jthread &T: tasks)
+            T.request_stop();
+    }
+
     void Test()
     {
-        std::stop_source source {};
+        std::stop_source source{};
         std::stop_token token = source.get_token();
 
         // std::thread job(func);
@@ -135,90 +162,97 @@ namespace StoppingThreads
         job.join();
 
     }
+}
 
-    void func1(const std::stop_token& st, int num)
+
+
+
+namespace StoppingThreads::Callbacks
+{
+    void MultipleCallbacks()
     {
-        const auto id = std::this_thread::get_id();
-        std::osyncstream(std::cout) << "call func1(" << num << ")\n";
+        std::jthread job = std::jthread([](const std::stop_token &token)
+        {
+            std::stop_callback cbOne(token, [] {
+                std::osyncstream(std::cout) << "\tCallback (one)   called: ID =  " << std::this_thread::get_id() << std::endl;
+            });
 
-        // register a first callback:
-        std::stop_callback cb1{st, [num, id] {
-            std::osyncstream(std::cout) << "- STOP1 requested in func(" << num
-                      << (id == std::this_thread::get_id() ? ")\n" :") in main thread\n");
-        }};
+            std::stop_callback cbTwo(token, [] {
+                std::osyncstream(std::cout) << "\tCallback (Two)   called: ID =  " << std::this_thread::get_id() << std::endl;
+            });
 
-        std::this_thread::sleep_for(9ms);
+            std::stop_callback cbThree(token, [] {
+                std::osyncstream(std::cout) << "\tCallback (Three) called: ID =  " << std::this_thread::get_id() << std::endl;
+            });
 
-        /*
-        // register a second callback:
-        std::stop_callback cb2{st, [num, id]{
-            std::cout << "- STOP2 requested in func(" << num
-                      << (id == std::this_thread::get_id() ? ")\n" : ") in main thread\n");
-        }};
-        std::this_thread::sleep_for(2ms);
-        */
-    }
-
-
-    void StopThread2()
-    {
-        // create stop_source and stop_token:
-        std::stop_source ssrc;
-        std::stop_token stok { ssrc.get_token() };
-
-        // register callback:
-        std::stop_callback cb{stok, []{
-            std::osyncstream(std::cout)<< "- STOP requested in main()\n" << std::flush;
-        }};
-
-        // in the background call func() a couple of times:
-        auto fut = std::async([stok] {
-            for (int num = 1; num < 13; ++num) {
-                func1(stok, num);
+            while (!token.stop_requested()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(250));
             }
+
+            std::osyncstream(std::cout) << "Thread id: " << std::this_thread::get_id() << " done\n";
         });
 
-        // after a while, request stop:
-        std::this_thread::sleep_for(120ms);
-        ssrc.request_stop();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::osyncstream(std::cout) << "Main  : Stopping thread.\n";
+        job.request_stop(); // request stop
+
+        job.join();
+        std::osyncstream(std::cout) << "Main  : Done.\n";
     }
+}
 
-
-    void StopThread3()
+namespace StoppingThreads::StopSource
+{
+    void task(const int id, const std::stop_token& token)
     {
-        auto func = [](std::stop_token token) {
-            std::atomic<int> counter{0};
-            auto thread_id = std::this_thread::get_id();
-            std::stop_callback callBack(token, [&counter, thread_id] {
-                std::osyncstream(std::cout) << "(Stop callback) id: " << thread_id << "; counter: " << counter << '\n';
-            });
-            while (counter < 10) {
-                std::this_thread::sleep_for(std::chrono::milliseconds (250));
-                ++counter;
+        const std::thread::id threadId = std::this_thread::get_id();
+        for (int i = 10; i; --i)
+        {
+            if (token.stop_requested()) {
+                std::osyncstream{std::cout} << "[" << threadId << " - " << id << "] stop request\n";
+                return;
             }
 
-            std::osyncstream(std::cout) << "Thread id: " << thread_id << " stopped actually now\n";
-        };
-
-        std::vector<std::jthread> tasks(10);
-        for (std::jthread & T: tasks)
-            T = std::jthread(func);
-
-        std::this_thread::sleep_for(1s);
-        for (std::jthread & T: tasks)
-            T.request_stop();
+            std::this_thread::sleep_for(250ms);
+            std::osyncstream{std::cout} << "[" << threadId << " - " << id << "] sleeping\n";
+        }
     }
-};
+
+    void printStopSourceInfo(const std::stop_source& source)
+    {
+        std::cout << "Stop Source info:\n"
+            << "\tstop_possible : " << std::boolalpha  <<  source.stop_possible() << "\n"
+            << "\tstop_requested: " << std::boolalpha  <<  source.stop_requested() << std::endl;
+    };
+
+    // A stop request made for one stop_source object is visible to all
+    // stop_sources and std::stop_tokens of the same associated
+
+    void StopMultipleThreads()
+    {
+        std::stop_source stopSource;
+        printStopSourceInfo(stopSource);
+
+        std::vector<std::jthread> pool;
+        for (int i = 0; i < 5; ++i)
+            pool.emplace_back(task, i, stopSource.get_token());
+
+        std::this_thread::sleep_for(500ms);
+        std::osyncstream{std::cout} << "Request stop\n";
+
+        stopSource.request_stop();
+        printStopSourceInfo(stopSource);
+    }
+}
 
 void StoppingThreads::TestAll()
 {
-    // SimpleExample();
-    // SimpleExample_StopCallback();
-    SimpleExample_ConditionalVariable();
+    // BasicExamples::SimpleExample();
+    // BasicExamples::SimpleExample_StopCallback();
+    // BasicExamples::SimpleExample_ConditionalVariable();
+    // BasicExamples::StopMultipleThreads();
 
-    // SimpleExample();
-    // StopThread2();
-    // Test();
+    // Callbacks::MultipleCallbacks();
 
-    //StopThread3();
+    StopSource::StopMultipleThreads();
 };
