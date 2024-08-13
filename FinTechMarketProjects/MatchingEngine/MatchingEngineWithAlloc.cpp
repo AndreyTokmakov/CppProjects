97262dd632e8b273eef6b5629dbe8553c532b464
AndreyTokmakov <andtokm@yandex.ru>
2024-08-13 20:43:40 +0400

    13_08_2024__04

--- FinTechMarketProjects/MatchingEngine/MatchingEngineWithAlloc.cpp
+++ FinTechMarketProjects/MatchingEngine/MatchingEngineWithAlloc.cpp
@@ -155,52 +155,9 @@ namespace Memory
     };
 }
 
-namespace MatchingEngine_WithAllocator
+namespace MatchingEngine_NO_WithAllocator
 {
     using namespace Common;
-
-    struct Trade
-    {
-        struct OrderInfo
-        {
-            Order::OrderID id = 0;
-            Order::OrderID price = 0;
-        };
-
-        OrderInfo buyOrderInfo;
-        OrderInfo sellOrderInfo;
-        uint32_t  quantity = 0;
-
-        static void addOrder(const Order& order, OrderInfo& orderInfo)
-        {
-            orderInfo.id = order.orderId;
-            orderInfo.price = order.price;
-        }
-
-        Trade& setBuyOrder(const Order& order) {
-            addOrder(order, buyOrderInfo);
-            return *this;
-        }
-
-        Trade& setSellOrder(const Order& order) {
-            addOrder(order, sellOrderInfo);
-            return *this;
-        }
-
-        Trade& setQuantity(const uint32_t qnty) {
-            quantity = qnty;
-            return *this;
-        }
-    };
-
-    struct Trades
-    {
-        std::vector<Trade> trades {};
-
-        Trade& addTrade() {
-            return trades.emplace_back();
-        }
-    };
 
     struct OrderMatchingEngine
     {
@@ -215,21 +172,13 @@ namespace MatchingEngine_WithAllocator
             PriceOrderList* priceLevelOrderList;
         };
 
+        Memory::ObjectPool<Order> ordersPool;
         std::list<Order> orders {};
+
         std::unordered_map<Order::IDType, ReferencesBlock> orderByIDMap;
 
-        // TODO: Test replace std::map --> boost::flat_map [std::list --> shall be pointer?]
-        //       Since look performance of this lookup is more critical one
-
-#if 0
-        std::map<Order::Price, PriceOrderList, std::less<>> buyOrders;
-        std::map<Order::Price, PriceOrderList, std::greater<>> sellOrders;
-#else
         boost::container::flat_map<Order::Price, PriceOrderList, std::less<>> buyOrders;
         boost::container::flat_map<Order::Price, PriceOrderList, std::greater<>> sellOrders;
-#endif
-
-        Trades trades;
 
         void processOrder(Order& order)
         {
@@ -248,57 +197,21 @@ namespace MatchingEngine_WithAllocator
 
         unsigned long long matchOrder(Order& order)
         {
-            if (OrderSide::SELL == order.side) {
-                matchOrder(order, buyOrders);
-            } else {
-                matchOrder(order, sellOrders);
-            }
-
-            // Return remaining quantity
-            return order.quantity;
+            // TODO:
+            return 1;
         }
 
         template<typename OrderSideMap>
         void matchOrder(Order& order, OrderSideMap& oppositeSideOrdersPriceMap)
         {
-            auto matchedPriceLevelIter = oppositeSideOrdersPriceMap.lower_bound(order.price);
-            while (oppositeSideOrdersPriceMap.end() != matchedPriceLevelIter && order.quantity > 0)
-            {
-                matchOrderList(order, matchedPriceLevelIter->second);
-                ++matchedPriceLevelIter;
-            }
+            // TODO:
         }
+
 
         void matchOrderList(Order& order,
                             PriceOrderList& matchedOrderList)
         {
-            for (auto orderIter = matchedOrderList.begin(); matchedOrderList.end() != orderIter;)
-            {
-                Order& matchedOrder = *(*orderIter);
-
-                Trade& trade = trades.addTrade();
-                trade.setQuantity(std::min(matchedOrder.quantity,order.quantity));
-                if (OrderSide::SELL == order.side) {
-                    trade.setBuyOrder(matchedOrder).setSellOrder(order);
-                } else {
-                    trade.setBuyOrder(order).setSellOrder(matchedOrder);
-                }
-
-                if (order.quantity >= matchedOrder.quantity)
-                {
-                    order.quantity -= matchedOrder.quantity;
-                    matchedOrder.quantity = 0;
-
-                    /** Deleting order **/
-                    orderByIDMap.erase(matchedOrder.orderId);
-                    matchedOrderList.erase(orderIter++);
-                } else {
-                    matchedOrder.quantity -= order.quantity;
-                    order.quantity = 0;
-                    ++orderIter;
-                    return;;
-                }
-            }
+            // TODO:
         }
 
         void handleOrderNew(Order& order)
@@ -307,10 +220,12 @@ namespace MatchingEngine_WithAllocator
                 return;
             }
 
-            const auto [iterOrderMap, inserted] = orderByIDMap.emplace(order.orderId, ReferencesBlock{});
+            const auto [iterOrderMap, inserted] = orderByIDMap.emplace(
+                    order.orderId, ReferencesBlock{});
             if (inserted)
             {
-                auto& [orderIter, priceOrderIter, priceLevelOrderList] = iterOrderMap->second;
+                auto& [orderIter, priceOrderIter, priceLevelOrderList] =
+                        iterOrderMap->second;
                 orderIter = orders.insert(orders.end(), order);
                 priceLevelOrderList = (OrderSide::BUY == order.side) ?
                                       &buyOrders[order.price] : &sellOrders[order.price];
@@ -318,79 +233,135 @@ namespace MatchingEngine_WithAllocator
             }
         }
 
-        void handleOrderCancel(const Order& order)
+        void handleOrderCancel(Order& order)
         {
-            if (const auto orderByIDIter = orderByIDMap.find(order.orderId);
-                    orderByIDMap.end() != orderByIDIter)
-            {
-                auto& [orderIter, priceOrderIter, priceLevelOrderList] = orderByIDIter->second;
-                priceLevelOrderList->erase(priceOrderIter);
-                orders.erase(orderIter);
-                orderByIDMap.erase(orderByIDIter);
-            }
+            // TODO:
         }
 
         void handleOrderAmend(Order& order)
         {
-            if (const auto orderByIDIter = orderByIDMap.find(order.orderId);
-                    orderByIDMap.end() != orderByIDIter)
-            {
-                Order& orderOriginal = *(orderByIDIter->second.orderIter);
-                if (orderOriginal.side != order.side) {
-                    return;
-                } else if (orderOriginal.price != order.price) {
-                    handleOrderCancel(order);
-                    handleOrderNew(order);
-                } else if (orderOriginal.price == order.price) {
-                    // TODO: update order parameters
-                    orderOriginal.quantity = order.quantity;
-                }
-            }
+            // TODO:
         }
 
         void info(bool printTrades = true)
         {
-            for (const auto& [orderId, orderIter]: orderByIDMap) {
-                Order& orderOne = *orderIter.orderIter;
-                Order& orderTwo = *(*orderIter.priceOrderIter);
-                if (orderId != orderOne.orderId || orderId != orderTwo.orderId) {
-                    std::cerr << "ERROR: ID: " << orderId << "!= " << orderOne.orderId << std::endl;
-                }
-            }
-
-            auto printOrders = [](const auto& orderMap) {
-                for (const auto& [price, ordersList]: orderMap) {
-                    std::cout << "\tPrice: [" << price << "]" << std::endl;
-                    for (const auto & orderIter: ordersList) {
-                        Common::printOrder(*orderIter);
-                    }
-                }
-            };
-
-            std::cout << "BUY:  " << std::endl; printOrders(buyOrders);
-            std::cout << "SELL: " << std::endl; printOrders(sellOrders);
-
-            std::cout << std::string(160, '=') << std::endl;
-            if (!printTrades)
-                return;
-            for (const auto& trade: trades.trades)
-            {
-                std::cout << "Trade(Buy: {id: " << trade.buyOrderInfo.id  << ", price: " << trade.buyOrderInfo.price << "}, "
-                          << "Sell: {id: " << trade.sellOrderInfo.id << ", price: " << trade.sellOrderInfo.price << "}, "
-                          << "quantity: " << trade.quantity << ")\n";
-            }
+            // TODO:
         }
     };
 }
 
-namespace MatchingEngine_WithAllocator::Tests
+namespace MatchingEngine_WithAllocator
+{
+    using namespace Common;
+
+
+    struct OrderMatchingEngine
+    {
+        using OrderPtr = std::unique_ptr<Order, Memory::ObjectPool<Order>::Deleter>;
+        using OrderPtrIter = typename std::list<OrderPtr>::iterator;
+        using PriceOrderList = std::list<OrderPtrIter>;
+        using PriceOrderIter = typename PriceOrderList::iterator;
+
+        struct ReferencesBlock
+        {
+            OrderPtrIter orderIter;
+            PriceOrderIter priceOrderIter;
+            PriceOrderList* priceLevelOrderList;
+        };
+
+        Memory::ObjectPool<Order> ordersPool;
+        std::list<OrderPtr> orders {};
+
+        std::unordered_map<Order::IDType, ReferencesBlock> orderByIDMap;
+
+        boost::container::flat_map<Order::Price, PriceOrderList, std::less<>> buyOrders;
+        boost::container::flat_map<Order::Price, PriceOrderList, std::greater<>> sellOrders;
+
+        OrderPtr makeOrder() {
+            return ordersPool.acquireObject();
+        }
+
+        void processOrder(OrderPtr&& order)
+        {
+            switch (order->action)
+            {
+                case OrderActionType::NEW:
+                    return handleOrderNew(std::move(order));
+                case OrderActionType::CANCEL:
+                    return handleOrderCancel(std::move(order));
+                case OrderActionType::AMEND:
+                    return handleOrderAmend(std::move(order));
+                default:
+                    return;
+            }
+        }
+
+        unsigned long long matchOrder(Order& order)
+        {
+            // TODO:
+            return 1;
+        }
+
+        template<typename OrderSideMap>
+        void matchOrder(Order& order, OrderSideMap& oppositeSideOrdersPriceMap)
+        {
+            // TODO:
+        }
+
+
+        void matchOrderList(Order& order,
+                            PriceOrderList& matchedOrderList)
+        {
+            // TODO:
+        }
+
+        void handleOrderNew(OrderPtr&& orderIn)
+        {
+            Order& order = *orderIn.get();
+            if (0 == matchOrder(order)) {
+                return;
+            }
+
+            const auto [iterOrderMap, inserted] = orderByIDMap.emplace(
+                    order.orderId, ReferencesBlock{});
+            if (inserted)
+            {
+                auto& [orderIter, priceOrderIter, priceLevelOrderList] =
+                        iterOrderMap->second;
+                orderIter = orders.insert(orders.end(), std::move(orderIn));
+                priceLevelOrderList = (OrderSide::BUY == order.side) ?
+                                      &buyOrders[order.price] : &sellOrders[order.price];
+                priceOrderIter = priceLevelOrderList->insert(priceLevelOrderList->end(), orderIter);
+            }
+        }
+
+        void handleOrderCancel(OrderPtr&& order)
+        {
+            // TODO:
+        }
+
+        void handleOrderAmend(OrderPtr&& order)
+        {
+            // TODO:
+        }
+
+        void info(bool printTrades = true)
+        {
+            // TODO:
+        }
+    };
+}
+
+
+namespace MatchingEngine_NO_WithAllocator::Tests
 {
 
     void Trade_SELL()
     {
-        /*
         OrderMatchingEngine engine;
-        for (int i = 0, price = 10; i < 10; ++i)
+
+        Utilities::ScopedTimer timer { "TEST"};
+        for (int i = 0, price = 10; i < 8'000'000; ++i)
         {
             if (price > 16)
                 price = 10;
@@ -403,29 +374,43 @@ namespace MatchingEngine_WithAllocator::Tests
 
             engine.processOrder(order);
         }
-        engine.info();
-        std::cout << std::string(160, '=') << std::endl;
 
+        std::cout << engine.ordersPool.size() << std::endl;
 
+    }
+}
+
+namespace MatchingEngine_WithAllocator::Tests
+{
+    void Trade_SELL()
+    {
+        OrderMatchingEngine engine;
+
+        Utilities::ScopedTimer timer { "TEST"};
+        for (int i = 0, price = 10; i < 8'000'000; ++i)
         {
-            Order order;
-            order.side = OrderSide::SELL;
-            order.price = 15;
-            order.quantity = 10;
-            order.orderId = getNextOrderID();
-            engine.processOrder(order);
+            if (price > 16)
+                price = 10;
+
+            OrderMatchingEngine::OrderPtr order = engine.makeOrder();
+            order->side = OrderSide::BUY;
+            order->price = price+=2;
+            order->quantity = 3;
+            order->orderId = getNextOrderID();
+
+            engine.processOrder(std::move(order));
         }
 
-        std::cout << std::string(160, '=') << std::endl;
+        std::cout << engine.ordersPool.size() << std::endl;
 
-        engine.info();
-        std::cout << std::string(160, '=') << std::endl;
-        */
     }
 }
 
 
 void MatchingEngine_WithAllocator_Tests()
 {
+    // using namespace MatchingEngine_NO_WithAllocator::Tests;
+    using namespace MatchingEngine_WithAllocator::Tests;
 
+    Trade_SELL();
 }
