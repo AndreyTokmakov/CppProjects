4d0a067942d9f8193024100cdbd2f425b5d06aa9
AndreyTokmakov <andtokm@yandex.ru>
2024-11-24 07:10:57 +0400

    24_11_2024__01

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1490,14 +1490,16 @@ int main([[maybe_unused]] int argc,
 
     // BitwiseOperations::test();
 
-    std::queue<int> m_queueData;
     /** * * * * *  Move to lib * * * * * **/
 
     // Coroutines::TestAll();
     // StackTrace::TestAll();
 
 
-    Cpp23_Features::TestAll();
+    int a[] = {1,2,3,4,5};
+    std::cout << sizeof(a) / sizeof (a[0]) << std::endl;
+
+    // Cpp23_Features::TestAll();
 
 
     // CompileTime_Programming::Factorial();
