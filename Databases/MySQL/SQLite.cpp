3fe307bc2980f7a0fe0087dbc3c255c4540d2fb0
Andrei.Tokmakov <andtokm@yandex.ru>
2025-01-05 20:17:50 +0400

    05_01_2025_04

--- Databases/MySQL/SQLite.cpp
+++ Databases/MySQL/SQLite.cpp
@@ -13,13 +13,23 @@ Description :
 #include <memory>
 #include <optional>
 #include <chrono>
+#include <filesystem>
 
 #include <sqlite3.h>
 
+namespace
+{
+    const std::string usersTableName = "users";
+    const std::string TIME_FORMAT = "%Y-%m-%d %H:%M:%S";
+
+}
 
 namespace SQLite
 {
     constexpr std::string_view dbPath { "/tmp/database.db" };
+
+    using SqlLiteConnection = std::unique_ptr<sqlite3, decltype(&sqlite3_close)>;
+    using SqlLiteStatement = std::unique_ptr<sqlite3_stmt, decltype(&sqlite3_finalize)>;
 
     struct UserDTO
     {
@@ -36,12 +46,64 @@ namespace SQLite
     };
 
 
-    void Test()
+    SqlLiteConnection establishConnection(const std::filesystem::path& dbPath)
     {
-        const std::unique_ptr<sqlite3, decltype(&sqlite3_close)> database { nullptr, sqlite3_close };
-        if (sqlite3* ptr = database.get(); SQLITE_OK != sqlite3_open(dbPath.data(), &(ptr)))
+        SqlLiteConnection connection { nullptr, sqlite3_close };
+        if (sqlite3* conn = nullptr; SQLITE_OK == sqlite3_open(dbPath.c_str(), &conn))
         {
-            std::cerr << "Failed to open database " << dbPath << std::endl;
+            connection.reset(conn);
+            return connection;
+        }
+
+        throw std::runtime_error("Could not open database '" + dbPath.string() + "'");
+    }
+
+    bool createTable(sqlite3* connection)
+    {
+        const std::string stmtStr = "CREATE TABLE " + usersTableName + " ("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,name TEXT,description TEXT,timestamp BIGINT);";
+
+        char* errorMessage { nullptr };
+        if (SQLITE_OK != sqlite3_exec(connection, stmtStr.c_str(), nullptr, nullptr, &errorMessage))
+        {
+            // std::cerr << "Failed to create table: [" << sqlite3_errmsg(connection) << "]"<< std::endl;
+            std::cerr << "Failed to create table: [" << errorMessage << "]"<< std::endl;
+            sqlite3_free(errorMessage);
+            return false;
+        }
+        return true;
+    }
+
+    bool insertUser(sqlite3* connection, const UserDTO& user)
+    {
+        const std::string stmtStr = std::string("INSERT INTO " + usersTableName +
+            " (name,description,timestamp)  VALUES (?,?,?);");
+
+        SqlLiteStatement statement { nullptr, &sqlite3_finalize };
+        if (sqlite3_stmt* stmt = nullptr;
+            SQLITE_OK == sqlite3_prepare_v2(connection, stmtStr.c_str(), -1, &stmt, nullptr))
+        {
+            statement.reset(stmt);
+
+            sqlite3_bind_text(statement.get(), 1, user.name.c_str(), -1, SQLITE_STATIC);
+            sqlite3_bind_text(statement.get(), 2, user.description.c_str(), -1, SQLITE_STATIC);
+            sqlite3_bind_int64(statement.get(), 3, user.timestamp.time_since_epoch().count());
+
+            if (SQLITE_DONE == sqlite3_step(statement.get()))
+            {
+                std::cout << "User created" << std::endl;
+                return true;
+            }
+            else
+            {
+                std::cerr << "Failed to create user" << std::endl;
+                return false;
+            }
+        }
+        else
+        {
+            std::cerr << "Failed to prepare statement: [" << stmtStr << "]" << std::endl;
+            return false;
         }
     }
 
@@ -50,6 +112,12 @@ namespace SQLite
 
 void SQLite::TestAll()
 {
-    Test();
+    const SqlLiteConnection dbConnection  =establishConnection(dbPath);
+
+    createTable(dbConnection.get());
+
+    insertUser(dbConnection.get(), UserDTO("John", "Doe"));
+    insertUser(dbConnection.get(), UserDTO("Max", "Doe"));
+    insertUser(dbConnection.get(), UserDTO("Bill", "Doe"));
 }
 
