5202aee27c54e5dcbdfb3f2a7623be17268df0d0
AndreyTokmakov <andtokm@yandex.ru>
2025-01-28 08:01:21 +0400

    28_01_2024__06

--- Multithreding/Coroutines/Coroutines.cpp
+++ Multithreding/Coroutines/Coroutines.cpp
@@ -16,7 +16,7 @@ Description : Coroutines.cpp
 #include <chrono>
 #include <coroutine>
 #include <generator>
-#include <iostream>
+#include <utility>
 
 #include "../Utilities/Utilities.h"
 
@@ -55,7 +55,7 @@ namespace Coroutines::SimpleCoroutine
             }
         };
 
-        explicit ReturnType(promise_type& promise) {
+        explicit ReturnType(promise_type&) {
             std::cout << "ReturnType()" << std::endl;
         }
 
@@ -339,7 +339,7 @@ namespace Coroutines::Fibonacci_Sequence_Generator_Ex
             Out output_data { };
 
             SequenceGenerator get_return_object() noexcept {
-                return SequenceGenerator{ *this };
+                return SequenceGenerator { *this };
             }
 
             void return_void() noexcept {
@@ -400,7 +400,6 @@ namespace Coroutines::Fibonacci_Sequence_Generator_Ex
         }
     }
 
-
     void Test()
     {
         SequenceGenerator<int64_t> fib = fibonacci();
@@ -432,6 +431,187 @@ namespace Coroutines::Fibonacci_Sequence_Generator_Ex
     }
 }
 
+namespace Coroutines::String_to_Integer_Parser
+{
+    using namespace std::string_literals;
+    using namespace std::chrono_literals;
+
+    template <typename Out, typename In>
+    struct AsyncParser
+    {
+        struct promise_type
+        {
+            std::optional<In> input_data { };
+            Out output_data { };
+
+            AsyncParser get_return_object() noexcept {
+                return AsyncParser { *this };
+            }
+
+            void return_void() noexcept {
+            }
+
+            std::suspend_always initial_suspend() noexcept {
+                return {};
+            }
+
+            std::suspend_always final_suspend() noexcept {
+                return {};
+            }
+
+            void unhandled_exception() noexcept {
+            }
+
+            std::suspend_always yield_value(int64_t num) noexcept {
+                output_data = num;
+                return {};
+            }
+
+            auto await_transform(char) noexcept {
+                struct awaiter {
+                    promise_type& promise;
+
+                    [[nodiscard]] bool await_ready() const noexcept {
+                        return promise.input_data.has_value();
+                    }
+
+                    [[nodiscard]] char await_resume() const noexcept {
+                        //assert (promise.input_data.has_value());
+                        return *std::exchange(promise.input_data, std::nullopt);
+                    }
+
+                    void await_suspend(std::coroutine_handle<promise_type>) const noexcept {
+                    }
+                };
+
+                return awaiter(*this);
+
+            }
+        };
+
+        std::coroutine_handle<promise_type> handle{};
+
+        explicit AsyncParser(promise_type& promise):
+                handle{ std::coroutine_handle<promise_type>::from_promise(promise)} {
+        }
+
+        ~AsyncParser() noexcept
+        {
+            if (handle) {
+                handle.destroy();
+            }
+        }
+
+        void put(char c) {
+            handle.promise().input_data = c;
+            if (!handle.done()) {
+                handle.resume();
+            }
+        }
+
+        int64_t get() {
+            if (!handle.done()) {
+                handle.resume();
+            }
+            return handle.promise().output_data;
+        }
+    };
+
+    AsyncParser<int64_t, char> parseStringAsynch()
+    {
+        while (true)
+        {
+            char c = co_await char{ };
+            int64_t number { }, sign { 1 };
+
+            if (c != '-' && c != '+' && !std::isdigit(c)) {
+                continue;
+            }
+
+            if (c == '-') {
+                sign = -1;
+            }
+            else if (std::isdigit(c)) {
+                number = number * 10 + c - '0';
+            }
+
+            while (true) {
+                c = co_await char{};
+                if (std::isdigit(c)) {
+                    number = number * 10 + c - '0';
+                }
+                else {
+                    break;
+                }
+            }
+
+            co_yield number * sign;
+        }
+    }
+
+    int64_t parseString(const std::string& str)
+    {
+        int64_t num { 0 }, sign { 1 };
+
+        std::size_t c = 0;
+        while (c < str.size()) {
+            if (str[c] == '-') {
+                sign = -1;
+            }
+            else if (std::isdigit(str[c])) {
+                num = num * 10ll + (str[c] - '0');
+            }
+            else if (str[c] == '#') {
+                break;
+            }
+            ++c;
+        }
+
+        return num * sign;
+    }
+
+    void Test()
+    {
+        const std::string num1 = "-123454321#"s;
+        const std::string num2 = "-123454321#98765#-20241337#+31415#"s;
+
+        // parse full string
+        std::cout << parseString(num1) << std::endl;
+
+        // parse the first half of the string
+        const std::size_t sz = num1.size();
+        AsyncParser<int64_t, char> num_parser = parseStringAsynch();
+        for (std::size_t i = 0; i < sz / 2; ++i) {
+            num_parser.put(num1[i]);
+        }
+
+        std::cout << "parsing number...\n"s;
+
+        // parse the second half of the string
+        for (std::size_t i = sz / 2; i < sz; ++i) {
+            num_parser.put(num1[i]);
+        }
+
+        // print the parsed number
+        std::cout << num_parser.get() << std::endl;
+
+        // now let's parse a string containing several numbers
+        for (char c : num2)
+        {
+            num_parser.put(c);
+
+            /// do something...
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(100u));
+
+            std::cout << num_parser.get() << std::endl;
+            /*
+            if (c == '#') {
+                std::cout << num_parser.get() << std::endl;
+            }*/
+        }
+    }
+}
 
 
 void Coroutines::TestAll()
@@ -441,5 +621,7 @@ void Coroutines::TestAll()
     // Waiting_Coroutine::Test();
 
     // Fibonacci_Sequence_Generator::Test();
-    Fibonacci_Sequence_Generator_Ex::Test();
+    // Fibonacci_Sequence_Generator_Ex::Test();
+
+    String_to_Integer_Parser::Test();
 }
