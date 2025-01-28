496d38da38df6df68494086508efcd8f231f8099
AndreyTokmakov <andtokm@yandex.ru>
2025-01-28 20:36:25 +0400

    28_01_2024__10

--- LinuxExperiments/UnixSockets/AsynchUnixSocketServer.cpp
+++ LinuxExperiments/UnixSockets/AsynchUnixSocketServer.cpp
@@ -12,6 +12,15 @@ Description : AsynchUnixSocketServer.cpp
 #include <cerrno>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
 
 #include <iostream>
 #include <string_view>
@@ -19,7 +28,8 @@ Description : AsynchUnixSocketServer.cpp
 #include <chrono>
 
 #define RESULT_SUCCESS   (0)
-#define INVALID_HANDLE  (-1)
+#define INVALID_HANDLE   (-1)
+#define SOCKET_ERROR     (-1)
 #define SERVER_SOCK_PATH "/tmp/unix_socket"
 
 namespace
@@ -60,6 +70,14 @@ namespace AsynchUnixSocketServer
         Socket serverSocket { INVALID_HANDLE };
         std::string filePath { SERVER_SOCK_PATH };
 
+        std::vector<Socket> clients;
+
+        // TODO: --> std::chrono
+        timeval timeout {};
+
+        std::array<char, 1024> buffer {};
+        std::string request;
+
         UDSAsynchServer()
         {
             serverSocket = ::socket(AF_UNIX, SOCK_STREAM, 0);
@@ -78,8 +96,7 @@ namespace AsynchUnixSocketServer
         ~UDSAsynchServer()
         {
             if (const int32_t result = ::remove(filePath.data());
-                INVALID_HANDLE == result)
-            {
+                INVALID_HANDLE == result) {
                 std::cerr << "remove() failed. Result = " << result << std::endl;
             }
 
@@ -104,36 +121,102 @@ namespace AsynchUnixSocketServer
             return true;
         }
 
+
+        // TODO: Rename
+        bool setListenerOptions() const
+        {
+            if (SOCKET_ERROR == ::fcntl(serverSocket, F_SETFL, O_NONBLOCK)) {
+                std::cerr << "FCNTL failed. Error = " << errno << std::endl;
+                return false;
+            }
+
+            int32_t yes { 1 };
+            if (SOCKET_ERROR == ::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))) {
+                std::cout << "Setsockopt failed. Error = " << errno << std::endl;
+                return false;
+            }
+            return true;
+        }
+
+        bool waitEvent(fd_set *fdSet)
+        {
+            const Socket maxClient = clients.empty() ? 0 : *max_element(clients.cbegin(), clients.cend());
+            const Socket mx = std::max(serverSocket, maxClient);
+
+            timeout.tv_sec = 3;
+
+            if (SOCKET_ERROR == ::select(mx + 1, fdSet, nullptr, nullptr, &timeout)) {
+                std::cout << "Select failed. Error = " << errno << std::endl;
+                return false;
+            }
+            return true;
+        }
+
+        bool handleConnection(const fd_set *readset)
+        {
+            if (FD_ISSET(this->serverSocket, readset))
+            {
+                std::cout << __FUNCTION__ << std::endl;
+
+                Socket clientSocket = ::accept(serverSocket, nullptr, nullptr);
+                if (INVALID_HANDLE == clientSocket) {
+                    std::cerr << "Failed to accept client connection. Error = " << errno << std::endl;
+                    return false;
+                }
+                fcntl(clientSocket, F_SETFL, O_NONBLOCK);
+                clients.push_back(clientSocket);
+                std::cout << "Client connection accepted (Clients in table " << clients.size() << ")" << std::endl;
+            }
+            return true;
+        }
+
+        bool handleRead(const fd_set *readset)
+        {
+            std::cout << __FUNCTION__  << std::endl;
+            for (Socket clientSocket: clients)
+            {
+                if (clients.empty()) {
+                    std::cout << "END! Break" << std::endl;
+                    break;
+                }
+                if (FD_ISSET(clientSocket, readset))
+                {
+                    std::cout << __FUNCTION__  << std::endl;
+                    const int64_t bytesRead = ::recv(clientSocket, buffer.data(), buffer.size(), 0);
+                    if (bytesRead <= 0)
+                    {
+                        std::erase_if(clients, [&](const auto sock) {
+                            return clientSocket == sock;
+                        });
+                        ::close(clientSocket);
+                        std::cout << "Connection closed (Clients in table " << clients.size() << ")" << std::endl;
+                        continue;
+                    }
+                    /*** Handle client request: ***/
+                    request.assign(buffer.data(), bytesRead);
+                    std::cout << request << std::endl;
+
+                    /*** Send response: ***/
+                    // ::send(clientSocket, const_cast<char *>(response.c_str()), response.length(), 0);
+                }
+            }
+            return true;
+        }
+
         void start()
         {
             std::cout << "SERVER: Socket listening..." << std::endl;
+            while (true) {
+                fd_set readset{};
+                FD_ZERO(&readset);
+                FD_SET (serverSocket, &readset);
 
-            sockaddr clientAddr {};
-            uint32_t len = sizeof(clientAddr);
-            const int clientSocket = ::accept(serverSocket, &clientAddr, (socklen_t*)&len);
-            if (INVALID_HANDLE == clientSocket) {
-                std::cerr << "SERVER: accept() failed" << std::endl;
-
-            } else {
-                std::cout << "SERVER: Connection request" << std::endl;
-            }
-            SocketGuard clientSocketGuard { clientSocket };
-
-            std::string buffer(1024, '\0');
-            const int64_t bytesReceived = ::recv(clientSocket, buffer.data(), buffer.size(), 0);
-            if (INVALID_HANDLE == bytesReceived) {
-                std::cerr << "SERVER: Error when receiving message() failed" << std::endl;
-            } else {
-                buffer.resize(bytesReceived);
-                std::cout << "SERVER: <== Data received: " << buffer << std::endl;
-            }
-
-            std::cout << "SERVER: ==> Sending the response..." << std::endl;
-            constexpr std::string_view message { "HELLO FROM SERVER" };
-            // const int64_t bytesSend = ::send(clientSocket, message.data(), message.size(), 0);
-            const int64_t bytesSend = ::write(clientSocket, message.data(), message.size());
-            if (INVALID_HANDLE == bytesSend) {
-                // error("SERVER: Error when sending message to client");
+                for (const Socket client: clients)
+                    FD_SET(client, &readset);
+                if (!waitEvent(&readset))
+                    break;
+                handleConnection(&readset);
+                handleRead(&readset);
             }
         }
     };
@@ -142,6 +225,9 @@ namespace AsynchUnixSocketServer
 void AsynchUnixSocketServer::TestAll()
 {
     UDSAsynchServer server;
+    if (!server.setListenerOptions()) {
+        std::cerr << "setListenerOptions failed" << std::endl;
+    }
     if (!server.bind()) {
         std::cerr << "Bind failed" << std::endl;
     }
