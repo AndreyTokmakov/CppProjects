24a5a6d210f85f9b7ea6d4ae2b8bd9d103b945d9
AndreyTokmakov <andtokm@yandex.ru>
2025-06-03 07:16:02 +0400

    03_07_2025_001

---
+++ OptimisationPerformanceTuning/SysCalls/main.cpp
@@ -0,0 +1,18 @@
+/**============================================================================
+Name        : main.cpp
+Created on  :
+Author      : Andrei Tokmakov
+Version     : 1.0
+Copyright   : Your copyright notice
+Description :
+============================================================================**/
+
+#include <iostream>
+
+int main([[maybe_unused]] int argc,
+         [[maybe_unused]] char** argv)
+{
+    std::cout << "Hello\n";
+
+    return 0;
+}
