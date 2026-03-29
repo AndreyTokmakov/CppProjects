d13e8113ca158d753b75d8a3eaad632bba05f825
AndreyTokmakov <andtokm@yandex.ru>
2026-03-29 20:43:29 +0400

    29_03_2026_9

--- DataStructures/Any/Any.cpp
+++ DataStructures/Any/Any.cpp
@@ -9,8 +9,71 @@ Description : Any.cpp
 
 #include "Any.hpp"
 
+#include <iostream>
+#include <format>
+#include <print>
+#include <memory>
+
+namespace
+{
+    class Any
+    {
+        struct Interface
+        {
+            virtual ~Interface() = default;
+            virtual void func() const noexcept  = 0;
+        };
+
+        template <typename Ty>
+        class Model : public Interface
+        {
+            Ty object;
+
+        public:
+
+            explicit Model(const Ty& obj) noexcept : object { obj } {
+            }
+
+            void func() const noexcept override {
+                object.func();
+            }
+        };
+
+        std::unique_ptr<Interface> implPtr;
+
+    public:
+
+        template <typename T>
+        explicit Any(T&& obj) noexcept :
+            implPtr { std::make_unique<Model<T>>(std::forward<T>(obj)) } {
+        }
+
+        void func() const noexcept {
+            implPtr->func();
+        }
+    };
+}
+
+
+namespace tests
+{
+    struct X
+    {
+        void func() const noexcept
+        {
+            std::cout << "X::func()" << std::endl;
+        }
+    };
+
+    void test()
+    {
+        const Any any {X {}};
+        any.func();
+    }
+}
 
 void any::TestAll()
 {
+    tests::test();
 
 }
