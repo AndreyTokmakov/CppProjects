568713fe2112f6dd42d8ed473af7f82ea57fe5bd
AndreyTokmakov <andtokm@yandex.ru>
2026-03-31 19:43:04 +0400

    31_03_2026_1

--- DataStructures/Any/Any.cpp
+++ DataStructures/Any/Any.cpp
@@ -13,67 +13,89 @@ Description : Any.cpp
 #include <format>
 #include <print>
 #include <memory>
+#include <vector>
+#include <atomic>
 
-namespace
+
+namespace any
 {
-    class Any
+    struct TypeId
     {
-        struct Interface
+        using type_id_t = uint16_t;
+
+        template<typename>
+        [[nodiscard]]
+        static type_id_t getId()
         {
-            virtual ~Interface() = default;
-            virtual void func() const noexcept  = 0;
-        };
-
-        template <typename Ty>
-        class Model : public Interface
-        {
-            Ty object;
-
-        public:
-
-            explicit Model(const Ty& obj) noexcept : object { obj } {
-            }
-
-            void func() const noexcept override {
-                object.func();
-            }
-        };
-
-        std::unique_ptr<Interface> implPtr;
-
-    public:
-
-        template <typename T>
-        explicit Any(T&& obj) noexcept :
-            implPtr { std::make_unique<Model<T>>(std::forward<T>(obj)) } {
+            static const type_id_t typeID = getUniqueId();
+            return typeID;
         }
 
-        void func() const noexcept {
-            implPtr->func();
+    private:
+
+        [[nodiscard]]
+        static type_id_t getUniqueId() noexcept
+        {
+            static std::atomic<uint32_t> currentTypeId { 0 };
+            return currentTypeId.fetch_add(1, std::memory_order::relaxed);
         }
     };
-}
 
-
-namespace tests
-{
-    struct X
+    struct Any
     {
-        void func() const noexcept
+        TypeId::type_id_t actualTypeId {};
+
+        Any() = default;
+
+        template <typename Ty>
+        explicit Any(Ty&& item)
         {
-            std::cout << "X::func()" << std::endl;
+            std::cout << typeid(item).name() << " ==> " << TypeId::getId<Ty>()  << std::endl;
         }
+
+        template <typename Ty>
+        [[nodiscard]]
+        bool containsType() const noexcept
+        {
+            return actualTypeId == TypeId::getId<Ty>();
+        }
+
+        template<typename Ty>
+        Any& operator=(const Ty& item)
+        {
+            std::cout << typeid(item).name() << " ==> " << TypeId::getId<Ty>() << std::endl;
+
+            constexpr size_t typeSize = sizeof(std::remove_reference_t<Ty>);
+            placeholder.resize(typeSize);
+            placeholder.shrink_to_fit();
+
+            // std::cout << placeholder.size() << std::endl;
+            auto* str = std::construct_at(reinterpret_cast<Ty*>(placeholder.data()), item);
+            // std::cout << *str << std::endl;
+
+            return *this;
+        }
+
+        std::vector<uint8_t> placeholder;
     };
 
     void test()
     {
-        const Any any {X {}};
-        any.func();
+        /*
+        {
+            Any any {int{}};
+            std::cout << std::boolalpha << any.containsType<int>() << ", " << any.containsType<double>() << std::endl;
+        }*/
+
+        Any any;
+
+        any = std::string{ "123456"};
+        //any = int{};
+        //any = std::string{};
     }
 }
 
 void any::TestAll()
 {
-    tests::test();
-
+    any::test();
 }
