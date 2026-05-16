12512d6883299fe589d4da2cf298237c714d29bf
AndreyTokmakov <andtokm@yandex.ru>
2026-05-16 20:01:39 +0400

    16_05_2026_02

--- OptimisationPerformanceTuning/main.cpp
+++ OptimisationPerformanceTuning/main.cpp
@@ -21,6 +21,7 @@ Description : C++ Optimisation and Performance Tuning experiments
 #include "Loop_Unrolling/Loop_Unrolling.hpp"
 
 #include <iostream>
+#include <print>
 #include <string_view>
 #include <vector>
 #include <thread>
@@ -57,11 +58,44 @@ namespace Variables_MemoryLayout
 }
 
 
+namespace caches
+{
+    size_t getCacheSizeL1_I() {
+        return ::sysconf(_SC_LEVEL1_ICACHE_SIZE);
+    }
+
+    size_t getCacheSizeL1_D() {
+        return ::sysconf(_SC_LEVEL1_DCACHE_SIZE);
+    }
+
+    size_t getCacheSizeL2(){
+        return ::sysconf(_SC_LEVEL2_CACHE_SIZE);
+    }
+
+    size_t getCacheSizeL3()
+    {
+        // Linux-specific; returns L3 size in bytes
+        const int64_t size = ::sysconf(_SC_LEVEL3_CACHE_SIZE);
+        return (size > 0) ? static_cast<size_t>(size) : 8 * 1024 * 1024;  // 8MB fallback
+    }
+
+    void printCPUCaches()
+    {
+        std::println("L1I {}", getCacheSizeL1_I());
+        std::println("L1D {}", getCacheSizeL1_D());
+        std::println("L2  {}", getCacheSizeL2());
+        std::println("L3  {}", getCacheSizeL3());
+    }
+
+}
+
 
 int main([[maybe_unused]] const int argc,
          [[maybe_unused]] char** argv)
 {
     const std::vector<std::string_view> args(argv + 1, argv + argc);
+
+    caches::printCPUCaches();
 
     // FalseSharingExperiments::TestAll();
     // Variables_MemoryLayout::TestAll();
@@ -74,7 +108,7 @@ int main([[maybe_unused]] const int argc,
     // AoS_vs_SoA::TestAll();
     // Remove_Branches_Builtin_Unreachable::TestAll();
     // BranchLessCodeExamples::TestAll();
-    loop_unrolling::TestAll();
+    // loop_unrolling::TestAll();
 
     return EXIT_SUCCESS;
 }
