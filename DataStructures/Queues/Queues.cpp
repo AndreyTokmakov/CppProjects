bf1b13c10568d438eee127650b9b9c9f865ab139
AndreyTokmakov <andtokm@yandex.ru>
2026-08-10 21:35:35 +0400

    10_08_2026_02

---
+++ DataStructures/Queues/Queues.cpp
@@ -0,0 +1,15 @@
+/**============================================================================
+Name        : Queues.cpp
+Created on  : 10.08.2026
+Author      : Andrei Tokmakov
+Version     : 1.0
+Copyright   : Your copyright notice
+Description : Queues.cpp
+============================================================================**/
+
+#include "Queues.hpp"
+
+void queues::TestAll()
+{
+    priority_dispatcher::TestAll();
+}
---
+++ DataStructures/Queues/Queues.hpp
@@ -0,0 +1,19 @@
+/**============================================================================
+Name        : Queues.hpp
+Created on  : 10.08.2026
+Author      : Andrei Tokmakov
+Version     : 1.0
+Copyright   : Your copyright notice
+Description : Queues.hpp
+============================================================================**/
+
+#ifndef CPPPROJECTS_QUEUES_HPP
+#define CPPPROJECTS_QUEUES_HPP
+
+namespace queues
+{
+    void TestAll();
+    namespace priority_dispatcher { void TestAll(); }
+}
+
+#endif //CPPPROJECTS_QUEUES_HPP
